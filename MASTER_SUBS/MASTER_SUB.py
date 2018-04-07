048841cc5eb462a862402525ac951cb6d010b38f
nishthapa <nishthapa@gmail.com>
2018-04-07 14:24:24 +0530

    Updated Client Class  Nomenclature

--- MASTER_SUBS/MASTER_SUB.py
+++ MASTER_SUBS/MASTER_SUB.py
@@ -27,19 +27,19 @@ QoS = 2
 BROKER_ADDRESS= "localhost"  #Broker address
 PORT_NUMBER = 1883                         #Broker port
  
-nrf_client = mqttClient.Client("MASTER_SUB")               #create new instance
+client = mqttClient.Client("MASTER_SUB")               #create new instance
 
-nrf_client.on_connect= on_connect                      #attach function to callback
-nrf_client.on_message= on_message                      #attach function to callback
+client.on_connect= on_connect                      #attach function to callback
+client.on_message= on_message                      #attach function to callback
  
-nrf_client.connect(BROKER_ADDRESS, PORT_NUMBER)          #connect to broker
+client.connect(BROKER_ADDRESS, PORT_NUMBER)          #connect to broker
  
-nrf_client.loop_start()        #start the loop
+client.loop_start()        #start the loop
  
 while Connected != True:    #Wait for connection
     time.sleep(0.1)
  
-nrf_client.subscribe([("rpi_gateway/nrf", QoS), ("rpi_gateway/esp8266", QoS), ("rpi_gateway/test_topic_1", QoS), ("rpi_gateway/test_topic_2", QoS)])
+client.subscribe([("rpi_gateway/nrf", QoS), ("rpi_gateway/esp8266", QoS), ("rpi_gateway/test_topic_1", QoS), ("rpi_gateway/test_topic_2", QoS)])
 time.sleep(1)
 
 try:
@@ -48,5 +48,5 @@ try:
  
 except KeyboardInterrupt:
     print ("exiting")
-    nrf_client.disconnect()
-    nrf_client.loop_stop()
+    client.disconnect()
+    client.loop_stop()
